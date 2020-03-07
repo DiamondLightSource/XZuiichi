f7051f226c1a7c45d06966adb7a290e714ecd7d4
ChrisOrr <45949926+co2e14@users.noreply.github.com>
2020-03-07 17:47:07 +0000

    comments on each section

--- crimp.py
+++ crimp.py
@@ -4,13 +4,13 @@
 @author: christianorr
 """
 import subprocess
-import time
+import time #used in testing
 import sys
 import os
 
+#Setup - probably a neater way of doing this...
 path = os.getcwd()
 print(path)
-
 inpnumber = input("How many datasets are there? ")
 inpnumberstatic = inpnumber
 inpnumberstaticint = int(inpnumber)
@@ -18,12 +18,14 @@ inpnumber = int(inpnumber)
 inpnumberline = inpnumber
 inpline = ("INPUT_FILE=")
 
+#Kill script if only one dataset to be given
 if (inpnumber > 1):
     print("")
 else:
     print("There is no point running crimp with only 1 input file...")
     sys.exit()
 
+#Write default XSCALE.INP commands -  can make this customisable in future
 defaults = ("OUTPUT_FILE=XSCALE.HKL",
 "RESOLUTION_SHELLS=10 5 4 3.6 3.4 3.2 3 2.8 2.6 2.5 2.4 2.3 2.2 2.1 2 1.9 1.8 1.7",
 "!INCLUDE_RESOLUTION_RANGE=",
@@ -31,21 +33,20 @@ defaults = ("OUTPUT_FILE=XSCALE.HKL",
 "!FRIEDEL'S_LAW=FALSE",
 "!REFLECTIONS/CORRECTION_FACTOR=50",
 "!STRICT_ABSORPTION_CORRECTION=TRUE")
-
-xscaleinp = open("XSCALE.INP","w")
-    
+xscaleinp = open("XSCALE.INP","w")    
 for line in defaults:
     xscaleinp.write(line)
     xscaleinp.write("\n")
 xscaleinp.close()
 
+#Set up the output log file
 xscaleout = open("XSCALEOUT.LP","w")
 xscaleout.write("Welcome to crimp!")
 xscaleout.write("\n")
 xscaleout.close()
 
+#Write the XSCALE input from user input
 xscaleinp = open("XSCALE.INP","a")
-
 while (inpnumberline > 0):
     inpnumberline = inpnumberline - 1
     dataline = input("Enter dataset: ")
@@ -70,17 +71,18 @@ if cont == "y":
 else:
     sys.exit()
 
+#Run XSCALE on input file and log output, delete last line of input file, repeat
 count = 1
 while (inpnumber > 0):
     inpnumber = inpnumber - 1
-    print("placeholder for xscale_par")
-    #subprocess.run(["xscale_par"])
+    print("placeholder for xscale_par")     #just for testing
+#    subprocess.run(["xscale_par"])     #remove '#' after testing
     xscalelp = open("XSCALE.LP","r")
     xscaleout = open("XSCALEOUT.LP","a")
     xscaleout.write(xscalelp.read())
     xscaleout.close()
     xscalelp.close()
-    time.sleep(10)
+    time.sleep(3)   #just for testing
     readFile = open("XSCALE.INP")
     lines = readFile.readlines()
     readFile.close()
@@ -96,4 +98,3 @@ while (inpnumber > 0):
         print("XSCALE complete")
 else:
     print("Processing finished")
-    
