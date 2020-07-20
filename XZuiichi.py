1d63e438396966d2f2354149465d887686780a31
ChrisOrr <45949926+co2e14@users.noreply.github.com>
2020-07-20 17:41:16 +0100

    PEP update
    conforms with PEP (apart from the graphics)

--- XZuiichi.py
+++ XZuiichi.py
@@ -3,13 +3,18 @@
 """
 @author: christianorr
 """
-import subprocess, sys, os
+import subprocess
+import sys
+import os
 from itertools import combinations
 
-# Python3 code to convert tuple into string 
-def convertTuple(tup): 
-    str =  ''.join(tup) 
+# Python3 code to convert tuple into string
+
+
+def convertTuple(tup):
+    str = ''.join(tup)
     return str
+
 
 print("""
 __   __ ______      _ _      _     _
@@ -22,7 +27,7 @@ __   __ ______      _ _      _     _
               C ORR 2019
               """)
 
-#Setup - probably a neater way of doing this...
+# Setup - probably a neater way of doing this...
 path = os.getcwd()
 print("You are here: " + path)
 print("""XZuiichi can test all possible (c)ombinations of the
@@ -37,7 +42,7 @@ inpnumber = int(input("How many datasets are there? "))
 inpnumberstatic = inpnumber
 inpline = ("INPUT_FILE=")
 
-#Kill script if only one dataset to be given
+# Kill script if only one dataset to be given
 if (inpnumber > 1):
     print("")
 else:
@@ -46,27 +51,33 @@ else:
 
 res = float(input("Resolution cutoff: "))
 resgaps = ((5 - res) / 11)
-shells = "10 5 "+str(round((res+(10*resgaps)), 1))+" "+ \
-str(round((res+(9*resgaps)), 1))+" "+str(round((res+(8*resgaps)), 1))+" "+ \
-str(round((res+(7*resgaps)), 1))+" "+str(round((res+(6*resgaps)), 1))+" "+ \
-str(round((res+(5*resgaps)), 1))+" "+str(round((res+(4*resgaps)), 1))+" "+ \
-str(round((res+(3*resgaps)), 1))+" "+str(round((res+(2*resgaps)), 1))+" "+ \
-str(round((res+resgaps), 1))+" "+str(round(res, 1))
-print("Because you gave a resolution of "+str(res)+" the resolution shells used are: "+str(shells))
+shells = ("10 5 " + str(round((res+(10 * resgaps)), 1)) + " " +
+          str(round((res + (9 * resgaps)), 1)) + " " +
+          str(round((res + (8 * resgaps)), 1)) + " " +
+          str(round((res + (7 * resgaps)), 1)) + " " +
+          str(round((res + (6 * resgaps)), 1)) + " " +
+          str(round((res + (5 * resgaps)), 1)) + " " +
+          str(round((res + (4 * resgaps)), 1)) + " " +
+          str(round((res + (3 * resgaps)), 1)) + " " +
+          str(round((res + (2 * resgaps)), 1)) + " " +
+          str(round((res + resgaps), 1)) + " " + str(round(res, 1)))
+print("Because you gave a resolution of " + str(res) +
+      " the resolution shells used are: "+str(shells))
 print("")
-quality = int(input("Score the diffraction quality 1-3 (1 is bad, 2 is okay, 3 is amazing): "))
+quality = int(input("""Score the diffraction quality 1-3
+                    (1 is bad, 2 is okay, 3 is amazing): """))
 
-#Set up the output log file
-xscaleout = open("XSCALEOUT.LP","w")
+# Set up the output log file
+xscaleout = open("XSCALEOUT.LP", "w")
 xscaleout.write("XZuiichi\n")
 xscaleout.close()
 
-#Write the XSCALE input from user input
-xscalePrep = open("XSCALEPREP.INP","w")
+# Write the XSCALE input from user input
+xscalePrep = open("XSCALEPREP.INP", "w")
 while (inpnumber > 0):
     inpnumber = inpnumber - 1
     dataline = input("Enter dataset: ")
-    xscalePrep = open("XSCALEPREP.INP","a")
+    xscalePrep = open("XSCALEPREP.INP", "a")
     xscalePrep.write(inpline)
     xscalePrep.write(dataline)
     xscalePrep.write("\n")
@@ -75,16 +86,16 @@ else:
     inpnumber = inpnumberstatic
     print("That's all the inputs I am expecting!")
 
-with open(dataline,'r') as infile:
-        for line in infile:
-            if line.startswith("!SPACE_GROUP_NUMBER="):
-                words = line.split()
-                sg = words[-1]
-                sg = int(sg)
-            if line.startswith("!X-RAY_WAVELENGTH="):
-                words = line.split()
-                wavelen = words[-1]
-                wavelen = float(wavelen)
+with open(dataline, 'r') as infile:
+    for line in infile:
+        if line.startswith("!SPACE_GROUP_NUMBER="):
+            words = line.split()
+            sg = words[-1]
+            sg = int(sg)
+        if line.startswith("!X-RAY_WAVELENGTH="):
+            words = line.split()
+            wavelen = words[-1]
+            wavelen = float(wavelen)
 
 if sg <= 2:
     sym = 1
@@ -104,14 +115,14 @@ if wavelen > 3:
 ref_corr_fact = sym * wav * quality * 3
 print("Using a reflection/correction factor of "+str(ref_corr_fact))
 
-#Write default XSCALE.INP commands -  can make this customisable in future
+# Write default XSCALE.INP commands -  can make this customisable in future
 defaults = ("OUTPUT_FILE=XSCALE.HKL",
-"RESOLUTION_SHELLS="+str(shells),
-"FRIEDEL'S_LAW=FALSE",
-"REFLECTIONS/CORRECTION_FACTOR="+str(ref_corr_fact),
-"STRICT_ABSORPTION_CORRECTION=TRUE")
-    
-#Prep input for permutations
+            "RESOLUTION_SHELLS="+str(shells),
+            "FRIEDEL'S_LAW=FALSE",
+            "REFLECTIONS/CORRECTION_FACTOR="+str(ref_corr_fact),
+            "STRICT_ABSORPTION_CORRECTION=TRUE")
+
+# Prep input for permutations
 if cut_or_comb == "c":
     xscalePrep = open("XSCALEPREP.INP")
     lineprep = xscalePrep.readlines()
@@ -119,47 +130,47 @@ if cut_or_comb == "c":
     print(len(lineprep))
     print("")
 
-#Loop through all combinations
+# Loop through all combinations
 if cut_or_comb == "c":
-    for size in range(2,len(lineprep)+1):
-        for i in combinations(lineprep,size):
+    for size in range(2, len(lineprep) + 1):
+        for i in combinations(lineprep, size):
             toRun = convertTuple(i)
-            xscaleinp = open("XSCALE.INP","w")    
+            xscaleinp = open("XSCALE.INP", "w")
             for line in defaults:
                 xscaleinp.write(line)
                 xscaleinp.write("\n")
             xscaleinp.write(toRun)
             xscaleinp.close()
             subprocess.run(["xscale_par"])
-            xscalelp = open("XSCALE.LP","r")
-            xscaleout = open("XSCALEOUT.LP","a")
+            xscalelp = open("XSCALE.LP", "r")
+            xscaleout = open("XSCALEOUT.LP", "a")
             xscaleout.write(xscalelp.read())
             xscaleout.close()
             xscalelp.close()
-        
-#Run XSCALE on input file and log output, delete last line of input file, repeat
+
+# XSCALE on input file and log output, delete last line of input file, repeat
 if cut_or_comb == "r":
     inpnumberline = inpnumber
-    xscaleinp = open("XSCALE.INP","a")
+    xscaleinp = open("XSCALE.INP", "a")
     for line in defaults:
         xscaleinp.write(line)
         xscaleinp.write("\n")
-    infiles = open("XSCALEPREP.INP","r")
+    infiles = open("XSCALEPREP.INP", "r")
     xscaleinp.write(infiles.read())
     infiles.close()
     xscaleinp.close()
     while (inpnumber > 0):
         inpnumber = inpnumber - 1
-        subprocess.run(["xscale_par"])      
-        xscalelp = open("XSCALE.LP","r")
-        xscaleout = open("XSCALEOUT.LP","a")
+        subprocess.run(["xscale_par"])
+        xscalelp = open("XSCALE.LP", "r")
+        xscaleout = open("XSCALEOUT.LP", "a")
         xscaleout.write(xscalelp.read())
         xscaleout.close()
         xscalelp.close()
         readFile = open("XSCALE.INP")
         lines = readFile.readlines()
         readFile.close()
-        xscaleinp = open("XSCALE.INP","w")
+        xscaleinp = open("XSCALE.INP", "w")
         xscaleinp.writelines([item for item in lines[:-1]])
         xscaleinp.close()
     else:
