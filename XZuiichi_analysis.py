c251129ee8f79468004004cd416885b82e08b758
ChrisOrr <45949926+co2e14@users.noreply.github.com>
2020-09-09 12:25:51 +0100

    work on dataframe multiindexing

--- XZuiichi_analysis.py
+++ XZuiichi_analysis.py
@@ -8,20 +8,8 @@ print(path)
 
 res1, res2, res3, res4, res5, res6, res7, res8, res9, res10, res11, res12, res13 = 10.0, 5.0, 4.8, 4.6, 4.5, 4.3, 4.1, 3.9, 3.7, 3.5, 3.4, 3.2, 3.0
 
-lp = input('Where is the LP file? ')
-print(lp)
+columnlist = ['res', 'obsref', 'uniref', 'posref', 'comp', 'robs', 'rexp', 'compar', 'isigi', 'rmeas', 'cchalf', 'anomcorr', 'sigano', 'nano', 'run']
 
-def analyse(lp_file, res):
-    with open (lp_file, 'r') as file, open((os.path.join(path, 'tempout.csv')), 'w') as out:
-        for line in file:
-            if line.lstrip().startswith(str(res) + '0'):
-                out.write(','.join(line.split()) + '\n')
-    with open((os.path.join(path, 'tempout.csv')), 'r') as file:
-        dataline = file.read().splitlines(True)
-    with open((os.path.join(path, 'out.csv')), 'a') as file:
-        file.writelines(dataline[:1])
+df = pd.read_csv('all.csv', index_col=[14, 0], names=columnlist)
 
-reslist = [res1, res2, res3, res4, res5, res6, res7, res8, res9, res10, res11, res12, res13]
-
-for i in reslist:
-    analyse(lp, i)
+print(df)
