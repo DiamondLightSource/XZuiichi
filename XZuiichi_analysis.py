ddf2a5ead118ea336bc8abd10cba5eac6b20b5f7
ChrisOrr <45949926+co2e14@users.noreply.github.com>
2020-10-26 13:52:00 +0000

    fin

--- XZuiichi_analysis.py
+++ XZuiichi_analysis.py
@@ -11,5 +11,10 @@ res1, res2, res3, res4, res5, res6, res7, res8, res9, res10, res11, res12, res13
 columnlist = ['res', 'obsref', 'uniref', 'posref', 'comp', 'robs', 'rexp', 'compar', 'isigi', 'rmeas', 'cchalf', 'anomcorr', 'sigano', 'nano', 'run']
 
 df = pd.read_csv('all.csv', index_col=[14, 0], names=columnlist)
+df = df.astype(float)
+#df = pd.to_numeric(df["anomcorr"], downcast='float')
 
 print(df)
+
+#result = df.loc[(df.index.get_level_values(('anomcorr') > 50))]
+
