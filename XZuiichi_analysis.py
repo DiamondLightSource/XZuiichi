e553b687bf178a45e0e61640a16e3e4147e2e28e
ChrisOrr <45949926+co2e14@users.noreply.github.com>
2020-10-29 16:24:37 +0000

    eugh!

--- XZuiichi_analysis.py
+++ XZuiichi_analysis.py
@@ -7,16 +7,20 @@ import matplotlib.pyplot as plt
 path = os.getcwd()
 print(path)
 combinations = 26
+resshel = [10.0, 5.00, 4.70, 4.50, 4.20, 3.90, 3.60, 3.40, 3.10, 2.80, 2.50, 2.30, 2.00]
 
-data = pd.read_csv("all.csv", header=None, engine='c')
-data.columns = ['res', 'obs', 'uniq', 'pos', 'completeness', 'robs', 'rexp', 'compared', 'isigi', 'rmeas', 'cchalf', 'anomcorr', 'sigano', 'nano', 'ident']
-
+data = pd.read_csv("all.csv", header=None, engine='c', usecols=[0, 4, 8, 9, 10, 11, 12, 13, 14])
+data.columns = ['res', 'completeness', 'isigi', 'rmeas', 'cchalf', 'anomcorr', 'sigano', 'nano', 'ident']
 data.set_index(['ident', 'res'], inplace=True)
-
 data.sort_index(inplace=True)
 
 for i in range(1, combinations, 1):
-    showme = data.loc[(i, 3.4), 'obs']
-    print(str(i) + " " + str(showme / 23))
-
-print(data)
+    for j in resshel:
+        comp = data.loc[(i, j), 'completeness']
+        print(comp)
+        isigi = data.loc[(i, j), 'isigi']
+        print(isigi)
+        rmeas = data.loc[(i, j), 'rmeas']
+        print(rmeas)
+        cchalf = data.loc[(i, j), 'cchalf']
+        print(cchalf)
